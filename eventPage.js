09b2f7431624ed50c371837565274c7a1a475cd3
Minjune Song <77006616+pythonlearner1025@users.noreply.github.com>
2021-11-05 14:34:40 -0400

    Update eventPage.js

--- eventPage.js
+++ eventPage.js
@@ -1,13 +1,14 @@
+// register contextmenu listener to chrome when it is launched
 chrome.runtime.onInstalled.addListener(()=>{
     chrome.contextMenus.create({
         id: 'clickObject',
         title: 'Add event to calendar',
         type: 'normal',
-    // context is when user selects on something (selection is type of user
-    // action, there may be others -- refer to chrome API)
         contexts: ['selection']
     });
 });
+// when contextmenu listener detects a click,
+// execute the following actions:
 chrome.contextMenus.onClicked.addListener(
     (info) => {
         chrome.tabs.create({
@@ -15,6 +16,4 @@ chrome.contextMenus.onClicked.addListener(
         });
     });
 
-/// register event listener on startup
-// chrome.action API>
 
